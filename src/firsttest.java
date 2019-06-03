615dee47a26627bd5a73af3dd2d239f2df52b63e
Paz Grego <paz.grego1@gmail.com>
2019-06-03 23:54:28 +0300

    feat(open-and-search): open github web site, search "security" word and press enter

--- src/firsttest.java
+++ src/firsttest.java
@@ -1,19 +1,25 @@
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
 
-public class firsttest {
+
+public class FirstTest {
     public static void main(String[] args) throws Exception {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "C://drivers//chromedriver.exe");
         driver.get("https://github.com/");
-        Thread.sleep(5000);
+        Thread.sleep(2000);
 
-        /*WebElement search = driver.findElement(By.tagName("input"));*/
-        WebElement search = driver.findElement(By.cssSelector("[name]= 'q'"));
-        search.click();
+          List<WebElement> search = driver.findElements(By.className("header-search-input"));
+            search.get(0).click();
+            search.get(0).sendKeys("security");
+            search.get(0).sendKeys(Keys.ENTER);
+            
+
     }
 }
 
