101e26e2c0a917f35a67de6da5cc690ba33299f5
Paz Grego <paz.grego1@gmail.com>
2019-06-04 14:02:49 +0300

    feat(find-description-element)

--- src/firsttest.java
+++ src/firsttest.java
@@ -3,8 +3,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 
 public class FirstTest {
@@ -12,16 +14,31 @@ public class FirstTest {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "C://drivers//chromedriver.exe");
         driver.get("https://github.com/");
+        Thread.sleep(3000);
+
+        WebElement search = driver.findElements(By.className("header-search-input")).get(0);
+        search.click();
+        search.sendKeys("security");
+        search.sendKeys(Keys.ENTER);
+
+        List <WebElement> searchResults = driver.findElements(By.xpath("//li[contains(@class, 'repo-list-item')]"));
+
+        for (WebElement elementResult : searchResults) {
+            String title = elementResult.findElement(By.tagName("h3")).getText();
+
+            String descriptionText = "No Description";
+            List <WebElement> descriptionList = elementResult.findElements(By.cssSelector(".col-12.col-md-9.d-inline-block.text-gray.mb-2.pr-4"));
+
+            if(descriptionList.size() > 0){
+              descriptionText = descriptionList.get(0).getText();
+             }
+           
+
+            System.out.println("title: " + title + " description: " + descriptionText);
+        }
+
         Thread.sleep(2000);
-
-          List<WebElement> search = driver.findElements(By.className("header-search-input"));
-            search.get(0).click();
-            search.get(0).sendKeys("security");
-            search.get(0).sendKeys(Keys.ENTER);
-
-        List<WebElement> Title = driver.findElements(By.xpath("//*[@class='repo-list']//h3"));
-        System.out.println(Title.get(0).getText());
-
+        driver.quit();
     }
 }
 
